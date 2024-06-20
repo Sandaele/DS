59085ce7d99c37560c266446d9f03b49dd11a11b
Sandaele <131976585+Sandaele@users.noreply.github.com>
2024-06-20 15:21:58 -0300

    MUDO
    MUDO

---
+++ CalculadoraJava2/src/calculadorajava2/CalculadoraJava2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorajava2;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class CalculadoraJava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
